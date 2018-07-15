a1e67994cd5419a1c11f85d1ef5f28dbf39807a9
kouichirou sasaki <gooses001@gmail.com>
2018-07-15 02:25:13 +0000

    login_test

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get '/blogs', to: 'blogs#main'
 
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users, only: [:new, :create, :show]
 end
